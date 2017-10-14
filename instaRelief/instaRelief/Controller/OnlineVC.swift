9e6ef3a90ce3d2a57dc478b9c950fec33cd3f3c1
Mark <markrassamni@me.com>
2017-10-14 04:09:03 -0700

    get locations

--- instaRelief/instaRelief/Controller/OnlineVC.swift
+++ instaRelief/instaRelief/Controller/OnlineVC.swift
@@ -8,16 +8,28 @@
 
 import UIKit
 import CoreLocation
+import Firebase
 
 class OnlineVC: UIViewController, CLLocationManagerDelegate {
     
+    
+    
+    
     let locationManager = CLLocationManager()
+    let GOOGLE_API_KEY = "AIzaSyButIsDYJy_Xau-VmvewsvH4e7H78KJF0s"
+    var ref: DatabaseReference!
+    var currentLocation: CLLocation?
+    
+    
+    @IBOutlet weak var reportButton: UIButton!
+    @IBOutlet weak var requestButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBestForNavigation
         locationManager.requestAlwaysAuthorization()
+        ref = Database.database().reference()
     }
     
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
@@ -54,9 +66,11 @@ class OnlineVC: UIViewController, CLLocationManagerDelegate {
 //    }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        let location = locations.last! as CLLocation
+//        let location = locations.last! as CLLocation
+        currentLocation = locations.last! as CLLocation
         
-        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+//        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        
         
 //        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
         
@@ -65,6 +79,32 @@ class OnlineVC: UIViewController, CLLocationManagerDelegate {
     
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
          print("Error: " + error.localizedDescription)
+    }
+    
+    func reportDanger(danger: String){
+        if let location = currentLocation {
+            let uuid = UIDevice.current.identifierForVendor!.uuidString
+            let date = Date()
+            let calendar = Calendar.current
+            let month = calendar.component(.month, from: date)
+            let day = calendar.component(.day, from: date)
+            let hour = calendar.component(.hour, from: date)
+            let minutes = calendar.component(.minute, from: date)
+            let seconds = calendar.component(.second, from: date)
+            let dateChild = "\(month) \(day) \(hour):\(minutes):\(seconds)"
+            self.ref.child("appUsers").child(uuid).child(dateChild).setValue(["latitude": location.coordinate.latitude])
+            self.ref.child("appUsers").child(uuid).child(dateChild).setValue(["longitude": location.coordinate.longitude])
+            self.ref.child("appUsers").child(uuid).child(dateChild).setValue(["danger": danger])
+        }
+        else {
+            // TODO: alert error no location available, try again later
+            self.locationManager.requestLocation()
+        }
+        
+    }
+    
+    func requestReport(){
+        // send location and phone number
     }
 
 
