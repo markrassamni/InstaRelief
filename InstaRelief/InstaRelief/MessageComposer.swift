c72546767ef61eea99602e026c79bb91cc4b2d96
Mark <markrassamni@me.com>
2017-10-13 20:55:45 -0700

    send texts

---
+++ InstaRelief/InstaRelief/MessageComposer.swift
@@ -0,0 +1,42 @@
+//
+//  MessageComposer.swift
+//  instaRelief
+//
+//  Created by Mark Rassamni on 10/13/17.
+//  Copyright © 2017 markrassamni. All rights reserved.
+//
+
+import Foundation
+import MessageUI
+
+let textMessageRecipients = ["1-619-304-5612"] 
+
+class MessageComposer: NSObject, MFMessageComposeViewControllerDelegate {
+    
+    // A wrapper function to indicate whether or not a text message can be sent from the user's device
+    func canSendText() -> Bool {
+        return MFMessageComposeViewController.canSendText()
+    }
+    
+    // Configures and returns a MFMessageComposeViewController instance
+    func configuredMessageComposeViewController() -> MFMessageComposeViewController {
+        let messageComposeVC = MFMessageComposeViewController()
+        messageComposeVC.messageComposeDelegate = self  //  Make sure to set this property to self, so that the controller can be dismissed!
+        messageComposeVC.recipients = textMessageRecipients
+        messageComposeVC.body = "Hey friend - Just sending a text message in-app using Swift!"
+        return messageComposeVC
+    }
+    
+    func configureTextMessage(text: String) -> MFMessageComposeViewController{
+        let messageComposeVC = MFMessageComposeViewController()
+        messageComposeVC.messageComposeDelegate = self
+        messageComposeVC.recipients = textMessageRecipients
+        messageComposeVC.body = text
+        return messageComposeVC
+    }
+    
+    // MFMessageComposeViewControllerDelegate callback - dismisses the view controller when the user is finished with it
+    func messageComposeViewController(_ controller: MFMessageComposeViewController, didFinishWith result: MessageComposeResult) {
+        controller.dismiss(animated: true, completion: nil)
+    }
+}
--- InstaRelief/InstaRelief/OfflineVC.swift
+++ InstaRelief/InstaRelief/OfflineVC.swift
@@ -1,3 +1,77 @@
+////
+////  OfflineVC.swift
+////  InstaRelief
+////
+////  Created by Mark Rassamni on 10/13/17.
+////  Copyright © 2017 markrassamni. All rights reserved.
+////
+//
+//import UIKit
+//
+//class OfflineVC: UIViewController, UITextFieldDelegate {
+//
+//    @IBOutlet weak var crossStreetsTxt: UITextField!
+//    @IBOutlet weak var cityTxt: UITextField!
+//    @IBOutlet weak var stateTxt: UITextField!
+//    @IBOutlet weak var countryTxt: UITextField!
+//    @IBOutlet weak var dangerTxt: UITextField!
+//
+//    @IBOutlet weak var dangerView: UIView!
+//    @IBOutlet weak var dangerPicker: UIPickerView!
+//
+//    let messageComposer = MessageComposer()
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        // Do any additional setup after loading the view, typically from a nib.
+//        dangerView.isHidden = true
+//    }
+//
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//        // Dispose of any resources that can be recreated.
+//    }
+//
+//    func textFieldDidBeginEditing(_ textField: UITextField) {
+//        if textField == dangerTxt {
+//            print("DANGER")
+//            dangerView.isHidden = false
+//            dangerTxt.isHidden = true
+//        }
+//    }
+//
+//    func textFieldDidEndEditing(_ textField: UITextField) {
+//        if textField == dangerTxt {
+//            dangerView.isHidden = true
+//            dangerTxt.isHidden = false
+//        }
+//    }
+//
+//    @IBAction func sendPressed(_ sender: Any) {
+//        // Make sure the device can send text messages
+//        if (messageComposer.canSendText()) {
+//            // Obtain a configured MFMessageComposeViewController
+////            let messageComposeVC = messageComposer.configuredMessageComposeViewController()
+//
+//            let text = "\(crossStreetsTxt)\\\(cityTxt)\\\(stateTxt)\\\(countryTxt)\\\(dangerTxt)"
+//            let messageComposeVC = messageComposer.configureTextMessage(text: text)
+//
+//            // Present the configured MFMessageComposeViewController instance
+//            // Note that the dismissal of the VC will be handled by the messageComposer instance,
+//            // since it implements the appropriate delegate call-back
+//            present(messageComposeVC, animated: true, completion: nil)
+//        } else {
+//            // Let the user know if his/her device isn't able to send text messages
+//            let errorAlert = UIAlertController(title: "Cannot Send Text Message", message: "Your device is not able to send text messages.", preferredStyle: .alert)
+//            errorAlert.show(self, sender: nil)
+//        }
+//    }
+//
+//
+//}
+//
+
+
 //
 //  OfflineVC.swift
 //  InstaRelief
@@ -8,22 +82,30 @@
 
 import UIKit
 
-class OfflineVC: UIViewController, UITextFieldDelegate {
+class OfflineVC: UIViewController, UITextFieldDelegate, UIPickerViewDelegate, UIPickerViewDataSource {
+    
     
     @IBOutlet weak var crossStreetsTxt: UITextField!
     @IBOutlet weak var cityTxt: UITextField!
     @IBOutlet weak var stateTxt: UITextField!
     @IBOutlet weak var countryTxt: UITextField!
     @IBOutlet weak var dangerTxt: UITextField!
-    
     @IBOutlet weak var dangerView: UIView!
     @IBOutlet weak var dangerPicker: UIPickerView!
     
+    var pickerDataSource = ["Fire", "Flood", "Zombie", "Hurricane", "Tornado"];
+    let messageComposer = MessageComposer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         dangerView.isHidden = true
+        dangerTxt.delegate = self
+        dangerPicker.delegate = self
+        dangerPicker.dataSource = self
+        cityTxt.delegate = self
+        stateTxt.delegate = self
+        countryTxt.delegate = self
     }
     
     override func didReceiveMemoryWarning() {
@@ -31,23 +113,69 @@ class OfflineVC: UIViewController, UITextFieldDelegate {
         // Dispose of any resources that can be recreated.
     }
     
-    func textFieldDidBeginEditing(_ textField: UITextField) {
+    //    func textFieldDidBeginEditing(_ textField: UITextField) {
+    //        dangerView.isHidden = false
+    //        dangerTxt.isHidden = true
+    //    }
+    
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
         if textField == dangerTxt {
-            print("DANGER")
-            dangerView.isHidden = false
             dangerTxt.isHidden = true
+            dangerView.isHidden = false
+            return false
         }
+        return true
     }
     
-    func textFieldDidEndEditing(_ textField: UITextField) {
-        if textField == dangerTxt {
-            dangerView.isHidden = true
-            dangerTxt.isHidden = false
-        }
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerDataSource.count
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
+        return pickerDataSource[row]
+    }
+    
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        dangerView.isHidden = true
+        dangerTxt.isHidden = false
+        dangerTxt.text = "\(pickerDataSource[row])"
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return "\(pickerDataSource[row])"
     }
     
     @IBAction func sendPressed(_ sender: Any) {
+//         Make sure the device can send text messages
+                if (messageComposer.canSendText()) {
+                    // Obtain a configured MFMessageComposeViewController
+        //            let messageComposeVC = messageComposer.configuredMessageComposeViewController()
         
+                    let text = "\(crossStreetsTxt)\\\(cityTxt)\\\(stateTxt)\\\(countryTxt)\\\(dangerTxt)"
+                    let messageComposeVC = messageComposer.configureTextMessage(text: text)
+        
+                    // Present the configured MFMessageComposeViewController instance
+                    // Note that the dismissal of the VC will be handled by the messageComposer instance,
+                    // since it implements the appropriate delegate call-back
+                    present(messageComposeVC, animated: true, completion: nil)
+                } else {
+                    // Let the user know if his/her device isn't able to send text messages
+                    let errorAlert = UIAlertController(title: "Cannot Send Text Message", message: "Your device is not able to send text messages.", preferredStyle: .alert)
+                    errorAlert.show(self, sender: nil)
+                }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print("Should return")
+//        self.view.endEditing(true)
+        textField.resignFirstResponder()
+        return true
+//        return false
     }
     
     
